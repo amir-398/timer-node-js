90bfcbcdc307ae82433704419c8c9d9de7cd8335
Amir <amir.398@hotmail.fr>
2023-12-12 16:47:38 +0100

    protect routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
+const jwtMiddleware = require("../middlewares/jwtMiddleware.js");
 
+// Route pour l'ajoute d'un utilisateur
 router.route("/register").post(userController.userRegister);
 
+// Route pour la connexion utilisateur
 router.route("/login").post(userController.userLogin);
 
-// Route pour la suppression d'un utilisateur
+// Route pour la suppression
 router
   .route("/:id")
-  .delete(userController.deleteUser)
-  .put(userController.updateUser);
+  .delete(jwtMiddleware.verifyToken, userController.deleteUser)
+  .put(jwtMiddleware.verifyToken, userController.updateUser);
 
 module.exports = router;
