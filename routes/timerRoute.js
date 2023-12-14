19ea0b472a94f3b08073234d23339d4e65935701
Amir <amir.398@hotmail.fr>
2023-12-14 10:00:21 +0100

    add get users time and average time

--- routes/timerRoute.js
+++ routes/timerRoute.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const timerController = require("../controllers/timerController");
+const jwtMiddleware = require("../middlewares/jwtMiddleware.js");
 // ajouter timer
-router.route("/:user_id/timer").post(timerController.createATimes);
+router
+  .route("/:user_id/timer")
+  .post(jwtMiddleware.verifyToken, timerController.createATimes)
+  .get(jwtMiddleware.verifyToken, timerController.getTimesByUser);
+
+router
+  .route("/:user_id/timer/avg")
+  .get(jwtMiddleware.verifyToken, timerController.getAverageTimeByUser);
 module.exports = router;
