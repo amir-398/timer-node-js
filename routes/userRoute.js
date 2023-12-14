410212f4e1daf6dc83d4dc240bdf3602997351b8
Amir <amir.398@hotmail.fr>
2023-12-14 14:21:17 +0100

    add swagger doc

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,110 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController.js");
-
 const jwtMiddleware = require("../middlewares/jwtMiddleware.js");
 
-// Route pour l'ajoute d'un utilisateur
+/**
+ * @openapi
+ * /users/register:
+ *   post:
+ *     summary: Enregistre un nouvel utilisateur
+ *     description: Crée un nouveau compte utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: boolean
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé avec succès
+ *       400:
+ *         description: Erreur de requête
+ */
+
 router.route("/register").post(userController.userRegister);
 
-// Route pour la connexion utilisateur
+/**
+ * @openapi
+ * /users/login:
+ *   post:
+ *     summary: Authentifie un utilisateur
+ *     description: Connecte un utilisateur en vérifiant son email et mot de passe
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Connexion réussie et token renvoyé
+ *       401:
+ *         description: Authentification échouée
+ */
+
 router.route("/login").post(userController.userLogin);
 
-// Route pour la suppression
+/**
+ * @openapi
+ * /users/{id}:
+ *   delete:
+ *     summary: Supprime un utilisateur
+ *     description: Supprime un utilisateur spécifié par son ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de l'utilisateur à supprimer
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Utilisateur supprimé avec succès
+ *       404:
+ *         description: Utilisateur non trouvé
+ *   put:
+ *     summary: Met à jour un utilisateur
+ *     description: Met à jour les informations d'un utilisateur spécifié par son ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID de l'utilisateur à mettre à jour
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: boolean
+ *     responses:
+ *       200:
+ *         description: Utilisateur mis à jour avec succès
+ *       404:
+ *         description: Utilisateur non trouvé
+ */
+
 router
   .route("/:id")
   .delete(jwtMiddleware.verifyToken, userController.deleteUser)
---
+++ swaggerConfig.js
@@ -0,0 +1,30 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Amir",
+        email: "amir.398@hotmail.fr",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+module.exports = swaggerDocs;
